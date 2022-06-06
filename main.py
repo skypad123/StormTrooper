d123d2e16723f9017006b0d5617aa1d61de38fa4
Sky Livingston <Skypad123@gmail.com>
2022-06-06 14:54:10 +0800

    simple script to fight NTU StarWars

---
+++ main.py
@@ -0,0 +1,92 @@
+
+from selenium import webdriver
+from datetime import datetime
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+
+#enter time to wait for
+starttime = "14:50:00"
+cdate = datetime.strftime(datetime.now(), "%d-%m-%Y")
+
+#enter time to login 
+logintime = "14:50:00"
+username = ""
+password = ""
+
+
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument('--log-level=3')
+
+# Provide the path of chromedriver present on your system.
+driver = webdriver.Chrome(executable_path="chromedriver",
+                            options=options)
+driver.set_window_size(1920,1080)
+
+# Send a get request to the url
+driver.get('https://wish.wis.ntu.edu.sg/pls/webexe/ldap_login.login?w_url=https://wish.wis.ntu.edu.sg/pls/webexe/aus_stars_planner.main')
+
+pressed = False
+while(not pressed):
+    print("waiting to login :" + str(datetime.now()))
+    
+    if (datetime.now() > datetime.strptime(cdate + " " + logintime, "%d-%m-%Y %H:%M:%S")):
+        # click submit button
+        try:
+            
+            #enter username box
+            elem = WebDriverWait(driver, 0.1).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/section[2]/div/div/center[1]/form/table/tbody/tr/td/table/tbody/tr[2]/td[2]/input"))
+            )
+            elem.send_keys(username)
+            #show password
+            elem = WebDriverWait(driver, 0.1).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/section[2]/div/div/center[1]/form/table/tbody/tr/td/table/tbody/tr[4]/td[2]/input[1]")) 
+            )
+            elem.click()
+
+            #enter password box
+            elem = WebDriverWait(driver, 0.1).until(
+            EC.presence_of_element_located((By.XPATH, "//html/body/div[3]/div/div/section[2]/div/div/form/center[1]/table/tbody/tr/td/table/tbody/tr[3]/td[2]/input"))
+            )
+            elem.send_keys(password)
+            #enter starplanner
+            elem = WebDriverWait(driver, 0.1).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/section[2]/div/div/form/center[1]/table/tbody/tr/td/table/tbody/tr[5]/td[2]/input[1]")) 
+            )
+            elem.click()
+        finally:
+            pressed = True
+
+
+
+pressed = False
+while(not pressed):
+    print("Waiting for add timing:" + str(datetime.now()))
+    
+    if (datetime.now() > datetime.strptime(cdate + " " + starttime, "%d-%m-%Y %H:%M:%S")):
+        # click submit button
+        try:
+            #submit_button = driver.find_elements_by_xpath("/html/body/form[2]/div/div[3]/div/div/section[2]/div/div/p/table/tbody/tr[1]/td[2]/table/tbody/tr[11]/td/form/input[1]")[0]
+            #submit_button.click()
+            
+            #add courses (critical point)
+            elem = WebDriverWait(driver, 0.1).until(
+            EC.presence_of_element_located((By.XPATH, "/html/body/form[2]/div/div[3]/div/div/section[2]/div/div/p/table/tbody/tr[1]/td[2]/table/tbody/tr[9]/td/form/input[1]"))
+            )
+            elem.click()
+
+
+            #comfirm courses (comment out for less speed more reliablility)
+            # elem = WebDriverWait(driver, 0.1).until(
+            # EC.presence_of_element_located((By.XPATH, "/html/body/form/div[3]/div/div/section[2]/div/div/input[1]")) #This is a dummy element
+            # )
+            # elem.click()
+        finally:
+            pressed = True
+       
+
+
